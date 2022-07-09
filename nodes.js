fecde084253b9d4e4d88f952f8f596b103a71185
Qa5im <24100256@lums.edu.pk>
2022-07-09 19:10:55 +0500

    changes

--- nodes.js
+++ nodes.js
@@ -10,54 +10,33 @@
         const sup_description_el= $(".sup_description")
         const sup_humidity_el= $(".sup_hummidity")
         const sup_windspeed_el= $(".sup_windspeed")
-    
+        const main_box_details_el= document.getElementsByClassName("main_box_detials")
+        const error_catcher_el= document.getElementById("error_catcher")
+
         let url= `https://api.openweathermap.org/data/2.5/weather?q=${input_el}&units=metric&appid=e7b490c1d9eb32f3d44b7496e9feb478`
-        
         fetch(url).then((res)=> res.json())
         .then( (data)=> 
         {
             let weather= data.weather[0].main
             let city= data.name
-            let url_2= "https://serpapi.com/search.json?q=" + city +" "+ weather + "&tbm=isch&ijn=0&api_key=99bf4e129b016858988984fe604afe416a278dad3d803a33571b92752da3e2f0"
-            try
-            {
-                fetch(url_2)
-                .then( (resp)=> resp.json() )
-                .then( (info)=>
-                {
-                    let maximum= 20
-
-                    let minimum=1
-                    let req_num= Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
-                    console.log(info.images_results[req_num].original)
-                    let link=info.images_results[req_num].original
-                    try
-                    {
-                        document.body.style.backgroundImage= `url(${link})`
-                    }
-                    catch(err)
-                    {
-                        console.log("catch it")
-                    }
-                       
-                }
-                )
-            }
-            catch
-            {
-                console.log("something went worinf")
-
-            }
-           
+            document.body.style.backgroundImage= "url('https://source.unsplash.com/1600x900/?" + city+" "+ weather+"' )";       
             details_city_el[0].innerText= `Weather in ${data.name}` 
             details_temp_el[0].innerText= Math.round(data.main.temp) +"°C"
             sup_icon_el.attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png` )       
             sup_description_el[0].innerText= data.weather[0].description
             sup_humidity_el[0].innerText= "Humidity: "+ data.main.humidity
             sup_windspeed_el[0].innerText= "Wind speed: " + data.wind.speed+ "km/h"
-            
-
+            main_box_details_el[0].style.display="block";
+            error_catcher_el.style.display="none";
         })
+        .catch
+        {
+            error_catcher_el.style.display="block";
+            main_box_details_el[0].style.display="none";
+        }
+
+    
+    
 
         input_main_box_el.val("")
 
